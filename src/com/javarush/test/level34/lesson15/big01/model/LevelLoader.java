0b96fd38ee08621820ad6e61a2aa5e5be3e3c6b2
kagarek <KagaeK@yandex.ru>
2016-03-25 17:35:42 +0200

    level34.lesson15.big01 - 16 - done

--- src/com/javarush/test/level34/lesson15/big01/model/LevelLoader.java
+++ src/com/javarush/test/level34/lesson15/big01/model/LevelLoader.java
@@ -30,51 +30,42 @@ public class LevelLoader
             int x0 = Model.FIELD_SELL_SIZE / 2;
             int y0 = Model.FIELD_SELL_SIZE / 2;
             String line = bufferedReader.readLine();
-            while (!line.startsWith("Maze: "+ level)){ line = bufferedReader.readLine(); }
-
-            String params_FileOffset = bufferedReader.readLine();
+            while (!line.startsWith("Maze: "+ level)) {
+                line = bufferedReader.readLine();
+            }
+            bufferedReader.readLine(); //FileOffset
             String params_sizeX = bufferedReader.readLine();
             String params_sizeY = bufferedReader.readLine();
-            String params_end = bufferedReader.readLine();
-            String params_Length = bufferedReader.readLine();
+            bufferedReader.readLine(); // End
+            bufferedReader.readLine(); // Length
             bufferedReader.readLine(); // for empty row
 
             int sizeX = Integer.parseInt(params_sizeX.substring(params_sizeX.indexOf(": ")+2));
             int sizeY = Integer.parseInt(params_sizeY.substring(params_sizeY.indexOf(": ")+2));
 
-            String[] objects;
-            for (int rows = 0; rows <= sizeY; rows++)
+            for (int rows = 0; rows < sizeY; rows++)
             {
                 line = bufferedReader.readLine();
-                objects = line.split("");
 
-                //if (objects.length==0) continue;
-                //int x = x0;
-
-                for (int cols = 0; cols < objects.length; cols++)
+                for (int cols = 0; cols < sizeX; cols++)
                 {
-                    String figure = objects[cols];
+                    String figure = line.substring(cols,cols+1);
                     switch (figure) {
                         case "X": {
                             walls.add(new Wall(x0 + Model.FIELD_SELL_SIZE * cols, y0 + Model.FIELD_SELL_SIZE * rows));
-                            //walls.add(new Wall(x,y0));
                             break;
                         }
                         case "*": {
                             boxes.add(new Box(x0 + Model.FIELD_SELL_SIZE * cols, y0 + Model.FIELD_SELL_SIZE * rows));
-                            //boxes.add(new Box(x,y0));
                             break;
                         }
                         case ".": {
                             homes.add(new Home(x0 + Model.FIELD_SELL_SIZE * cols, y0 + Model.FIELD_SELL_SIZE * rows));
-                            //homes.add(new Home(x,y0));
                             break;
                         }
                         case "&": {
                             boxes.add(new Box(x0 + Model.FIELD_SELL_SIZE * cols, y0 + Model.FIELD_SELL_SIZE * rows));
                             homes.add(new Home(x0 + Model.FIELD_SELL_SIZE * cols, y0 + Model.FIELD_SELL_SIZE * rows));
-                            //boxes.add(new Box(x,y0));
-                            //homes.add(new Home(x,y0));
                             break;
                         }
                         case "@": {
@@ -82,9 +73,7 @@ public class LevelLoader
                             break;
                         }
                     }
-                    //x = x + Model.FIELD_SELL_SIZE;
                 }
-                //y0 = y0 + Model.FIELD_SELL_SIZE;
             }
 
         }
