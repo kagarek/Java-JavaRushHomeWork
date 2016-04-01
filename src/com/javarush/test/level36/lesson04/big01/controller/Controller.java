308f84f6b22ec0e0fb0fd49da4275f400d7d802a
kagarek <KagaeK@yandex.ru>
2016-04-01 16:34:12 +0300

    level36.lesson04.big01 - task 3

--- src/com/javarush/test/level36/lesson04/big01/controller/Controller.java
+++ src/com/javarush/test/level36/lesson04/big01/controller/Controller.java
@@ -1,21 +1,28 @@
 package com.javarush.test.level36.lesson04.big01.controller;
 
 import com.javarush.test.level36.lesson04.big01.model.Model;
+import com.javarush.test.level36.lesson04.big01.view.UsersView;
 
 /**
  * Created by Igor_Makarychev on 01.04.2016.
  */
 public class Controller
 {
+    private UsersView usersView;
+    private Model model;
+
     public void setModel(Model model)
     {
         this.model = model;
     }
 
-    private Model model;
-
     public void onShowAllUsers() {
         model.loadUsers();
+        usersView.refresh(model.getModelData());
     }
 
+    public void setUsersView(UsersView usersView)
+    {
+        this.usersView = usersView;
+    }
 }
