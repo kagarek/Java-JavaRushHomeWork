81e1885339774f34d2ae46d8618151f5dd6663ec
kagarek <KagareK@yandex.ru>
2016-05-13 23:08:23 +0300

    Structure changes

---
+++ src/com/javarush/test/level36/lesson04/big01/model/MainModel.java
@@ -0,0 +1,57 @@
+package com.javarush.test.level36.lesson04.big01.model;
+
+import com.javarush.test.level36.lesson04.big01.bean.User;
+import com.javarush.test.level36.lesson04.big01.model.service.UserService;
+import com.javarush.test.level36.lesson04.big01.model.service.UserServiceImpl;
+
+import java.util.List;
+
+/**
+ * Created by Igor_Makarychev on 01.04.2016.
+ */
+public class MainModel implements Model
+{
+    private UserService userService = new UserServiceImpl();
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData()
+    {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers()
+    {
+        modelData.setDisplayDeletedUserList(false);
+        modelData.setUsers(getActiveUsers(userService.getUsersBetweenLevels(1,100)));
+    }
+
+    public void loadDeletedUsers() {
+        List<User> users = userService.getAllDeletedUsers();
+        modelData.setUsers(users);
+        modelData.setDisplayDeletedUserList(true);
+    }
+
+    public void loadUserById(long userId) {
+        User user = userService.getUsersById(userId);
+        modelData.setActiveUser(user);
+        modelData.setDisplayDeletedUserList(false);
+    }
+
+    public void deleteUserById(long id){
+        userService.deleteUser(id);
+        modelData.setDisplayDeletedUserList(false);
+        modelData.setUsers(getActiveUsers(userService.getUsersBetweenLevels(1,100)));
+    }
+
+    private List<User> getActiveUsers(List<User> userList){
+        return userService.filterOnlyActiveUsers(userList);
+    }
+
+    public void changeUserData(String name, long id, int level){
+        userService.createOrUpdateUser(name, id, level);
+        modelData.setDisplayDeletedUserList(false);
+        modelData.setUsers(getActiveUsers(userService.getUsersBetweenLevels(1,100)));
+    }
+}
