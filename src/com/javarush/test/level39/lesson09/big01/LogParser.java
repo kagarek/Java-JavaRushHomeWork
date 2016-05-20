3216b0d9ef84e1a8ab832d1ae2393a2b30fb44ab
kagarek <KagaeK@yandex.ru>
2016-05-20 18:40:05 +0300

    level39.lesson09.big01 - task 1 - undone

--- src/com/javarush/test/level39/lesson09/big01/LogParser.java
+++ src/com/javarush/test/level39/lesson09/big01/LogParser.java
@@ -2,73 +2,178 @@ package com.javarush.test.level39.lesson09.big01;
 
 import com.javarush.test.level39.lesson09.big01.query.IPQuery;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.*;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 
 public class LogParser implements IPQuery {
-    private ArrayList<File> logList = new ArrayList<>();
+    private ArrayList<LogEntity> logRecords = new ArrayList<>();
 
-    public LogParser(Path logDir) {
-        try {
-            Files.walk(Paths.get(logDir.toString())).forEach(filePath -> {
-                if (filePath.toString().endsWith(".log")) {
-                    this.logList.add(new File(filePath.toString()));
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
+    class LogEntity {
+        String ip;
+        String user;
+        Date date;
+        Event event;
+        Status status;
+
+        public LogEntity(String ip, String user, String date, String event, String status)
+        {
+            this.ip = ip;
+            this.user = user;
+            SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+            try
+            {
+                this.date = sdf.parse(date);
+            }
+            catch (ParseException e)
+            {
+                e.printStackTrace();
+            }
+            this.event = event.indexOf(" ") > 0 ? Event.valueOf(event.substring(0,event.indexOf(" "))) : Event.valueOf(event);
+            this.status = Status.valueOf(status);
         }
+
+        public String getIp() { return ip; }
+        public String getUser() { return user; }
+        public Date getDate() { return date; }
+        public Event getEvent() { return event; }
+        public Status getStatus() {return status; }
     }
 
-    /*
-1.2.1.	Метод getNumberOfUniqueIPs(Date after, Date before) должен возвращать
-количество уникальных IP адресов за выбранный период. Здесь и далее,
-если в методе есть параметры Date after и Date before, то нужно возвратить
-данные касающиеся только данного периода (включая даты after и before).
-Если параметр after равен null, то нужно обработать все записи, у которых
-дата меньше или равна before.
-Если параметр before равен null, то нужно обработать все записи, у которых
-дата больше или равна after.
-Если и after, и before равны null, то нужно обработать абсолютно все записи
-(без фильтрации по дате).
-1.2.2.	Метод getUniqueIPs() должен возвращать множество, содержащее все
-неповторяющиеся IP. Тип в котором будем хранить IP будет String.
-1.2.3.	Метод getIPsForUser() должен возвращать IP, с которых работал
-переданный пользователь.
-1.2.4.	Метод getIPsForEvent() должен возвращать IP, с которых было произведено
-переданное событие.
-1.2.5.	Метод getIPsForStatus() должен возвращать IP, события с которых
-закончилось переданным статусом.
-     */
+    public LogParser(Path logDir) {
+        try
+        {
+            File logFolder = new File(logDir.toString());
+            File[] logFileList = logFolder.listFiles();
+
+            for (File aLogFileList : logFileList)
+            {
+                if (aLogFileList.isFile() && aLogFileList.toString().endsWith(".log"))
+                {
+                    try (BufferedReader br = new BufferedReader(new FileReader(aLogFileList));)
+                    {
+                        String s;
+                        while ((s = br.readLine()) != null)
+                        {
+                            String[] logString = s.split("\t");
+                            String ip = logString[0];
+                            String user = logString[1];
+                            String dateString = logString[2];
+                            String eventString = logString[3];
+                            String statusString = logString[4];
+                            logRecords.add(new LogEntity(ip, user, dateString, eventString, statusString));
+                        }
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        catch (Exception e) {e.printStackTrace();}
+    }
+
 
     @Override
     public int getNumberOfUniqueIPs(Date after, Date before) {
-        return 0;
+        return getUniqueIPs(after, before).size();
     }
 
     @Override
     public Set<String> getUniqueIPs(Date after, Date before) {
-        return null;
+        Set<String> uniqueIPs = new HashSet<>();
+
+        for (LogEntity logEntity : logRecords)
+        {
+            if (!uniqueIPs.contains(logEntity.getIp()))
+            {
+                if (after == null) {
+                    if (before == null) uniqueIPs.add(logEntity.getIp());
+                    else if (logEntity.getDate().before(before)) uniqueIPs.add(logEntity.getIp());
+                }
+                else
+                    if (before == null) {
+                        if (logEntity.getDate().after(after)) uniqueIPs.add(logEntity.getIp());
+                    }
+                    else if (logEntity.getDate().after(after) && logEntity.getDate().before(before)) uniqueIPs.add(logEntity.getIp());
+            }
+        }
+        return uniqueIPs;
     }
 
     @Override
     public Set<String> getIPsForUser(String user, Date after, Date before) {
-        return null;
+
+        Set<String> IPsForUser = new HashSet<>();
+
+        for (LogEntity logEntity : logRecords)
+        {
+            if (!IPsForUser.contains(logEntity.getIp()) && logEntity.getUser().equalsIgnoreCase(user))
+            {
+                if (after == null) {
+                    if (before == null) IPsForUser.add(logEntity.getIp());
+                    else if (logEntity.getDate().before(before)) IPsForUser.add(logEntity.getIp());
+                }
+                else
+                if (before == null) {
+                    if (logEntity.getDate().after(after)) IPsForUser.add(logEntity.getIp());
+                }
+                else if (logEntity.getDate().after(after) && logEntity.getDate().before(before)) IPsForUser.add(logEntity.getIp());
+            }
+        }
+        return IPsForUser;
     }
 
     @Override
     public Set<String> getIPsForEvent(Event event, Date after, Date before) {
-        return null;
+
+        Set<String> IPsForEvent = new HashSet<>();
+
+        for (LogEntity logEntity : logRecords)
+        {
+            if (!IPsForEvent.contains(logEntity.getIp()) && event.equals(logEntity.getEvent()))
+            {
+                if (after == null) {
+                    if (before == null) IPsForEvent.add(logEntity.getIp());
+                    else if (logEntity.getDate().before(before)) IPsForEvent.add(logEntity.getIp());
+                }
+                else
+                if (before == null) {
+                    if (logEntity.getDate().after(after)) IPsForEvent.add(logEntity.getIp());
+                }
+                else if (logEntity.getDate().after(after) && logEntity.getDate().before(before)) IPsForEvent.add(logEntity.getIp());
+            }
+        }
+        return IPsForEvent;
     }
 
     @Override
     public Set<String> getIPsForStatus(Status status, Date after, Date before) {
-        return null;
+
+        Set<String> IPsForStatus = new HashSet<>();
+
+        for (LogEntity logEntity : logRecords)
+        {
+            if (!IPsForStatus.contains(logEntity.getIp()) && status.equals(logEntity.getStatus()))
+            {
+                if (after == null) {
+                    if (before == null) IPsForStatus.add(logEntity.getIp());
+                    else if (logEntity.getDate().before(before)) IPsForStatus.add(logEntity.getIp());
+                }
+                else
+                if (before == null) {
+                    if (logEntity.getDate().after(after)) IPsForStatus.add(logEntity.getIp());
+                }
+                else if (logEntity.getDate().after(after) && logEntity.getDate().before(before)) IPsForStatus.add(logEntity.getIp());
+            }
+        }
+
+        return IPsForStatus;
     }
 }
--- src/com/javarush/test/level39/lesson09/big01/Solution.java
+++ src/com/javarush/test/level39/lesson09/big01/Solution.java
@@ -1,11 +1,49 @@
 package com.javarush.test.level39.lesson09.big01;
 
 import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Solution {
     public static void main(String[] args) {
-        LogParser logParser = new LogParser(Paths.get("/Users/igormakarychev/Documents/Java projects/Java-JavaRushHomeWork/src/com/javarush/test/level39/lesson09/big01/logs"));
-        System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
+        LogParser logParser = new LogParser(Paths.get("./src/com/javarush/test/level39/lesson09/big01/logs"));
+        DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+        Date after = null;
+        try
+        {
+            after = df.parse("01.01.2016");
+        }
+        catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
+        Date before = new Date();
+
+        System.out.println(logParser.getNumberOfUniqueIPs(null, null));
+        System.out.println(logParser.getNumberOfUniqueIPs(after, null));
+        System.out.println(logParser.getNumberOfUniqueIPs(null, before));
+        System.out.println(logParser.getNumberOfUniqueIPs(after, before));
+
+        System.out.println(logParser.getUniqueIPs(null, null));
+        System.out.println(logParser.getUniqueIPs(after, null));
+        System.out.println(logParser.getUniqueIPs(null, before));
+        System.out.println(logParser.getUniqueIPs(after, before));
+
+        System.out.println(logParser.getIPsForEvent(Event.DONE_TASK,null, null));
+        System.out.println(logParser.getIPsForEvent(Event.DOWNLOAD_PLUGIN,after, null));
+        System.out.println(logParser.getIPsForEvent(Event.LOGIN,null, before));
+        System.out.println(logParser.getIPsForEvent(Event.SOLVE_TASK,after, before));
+
+        System.out.println(logParser.getIPsForStatus(Status.ERROR,null, null));
+        System.out.println(logParser.getIPsForStatus(Status.FAILED,after, null));
+        System.out.println(logParser.getIPsForStatus(Status.OK,null, before));
+        System.out.println(logParser.getIPsForStatus(Status.OK,after, before));
+
+        System.out.println(logParser.getIPsForUser("Eduard Petrovich Morozko",null, null));
+        System.out.println(logParser.getIPsForUser("Amigo",after, null));
+        System.out.println(logParser.getIPsForUser("Eduar Petrovi Morozk",null, before));
+        System.out.println(logParser.getIPsForUser("Vasya Pupkin",after, before));
     }
 }
